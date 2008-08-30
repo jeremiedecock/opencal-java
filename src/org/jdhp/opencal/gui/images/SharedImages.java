8a980987c88eb77fa081b704f51b08fb03e995a2
gremy <gremy@ecfe46be-1134-41ce-ba06-f4350a42f343>
2008-08-30 14:15:16 +0000

    Move images into the appropriate directory (x22 or x24) and add two images.
    git-svn-id: file:///media/data/gremy/svn/priv/repositories/svn/projet_0002/java/trunk@188 ecfe46be-1134-41ce-ba06-f4350a42f343

--- src/org/jdhp/opencal/gui/images/SharedImages.java
+++ src/org/jdhp/opencal/gui/images/SharedImages.java
@@ -5,17 +5,27 @@ import org.jdhp.opencal.gui.MainWindow;
 
 public class SharedImages {
 
-	public static final String EDIT_FIND = "edit-find.png";
-	public static final String FACE_SAD = "face-sad.png";
-	public static final String FACE_SMILE = "face-smile.png";
-	public static final String GO_FIRST = "go-first.png";
-	public static final String GO_LAST = "go-last.png";
-	public static final String GO_NEXT = "go-next.png";
-	public static final String GO_PREVIOUS = "go-previous.png";
-	public static final String LIST_ADD = "list-add.png";
-	public static final String LIST_REMOVE = "list-remove.png";
-	public static final String MEDIA_FLOPPY = "media-floppy.png";
+	// 22x22 images
+	public static final String EXIT = "x22/exit.png";
+	public static final String PRINT = "x22/print.png";
 	
+	// 24x24 images
+	public static final String EDIT_FIND = "x24/edit-find.png";
+	public static final String FACE_SAD = "x24/face-sad.png";
+	public static final String FACE_SMILE = "x24/face-smile.png";
+	public static final String GO_FIRST = "x24/go-first.png";
+	public static final String GO_LAST = "x24/go-last.png";
+	public static final String GO_NEXT = "x24/go-next.png";
+	public static final String GO_PREVIOUS = "x24/go-previous.png";
+	public static final String LIST_ADD = "x24/list-add.png";
+	public static final String LIST_REMOVE = "x24/list-remove.png";
+	public static final String MEDIA_FLOPPY = "x24/media-floppy.png";
+
+	/**
+	 * 
+	 * @param name
+	 * @return
+	 */
 	public static Image getImage(String name) {
 		return new Image(MainWindow.DISPLAY, SharedImages.class.getResourceAsStream(name));
 	}
