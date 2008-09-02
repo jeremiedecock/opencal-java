72445c6f66618c72dd28123e76b2cf86fcea31e8
gremy <gremy@ecfe46be-1134-41ce-ba06-f4350a42f343>
2008-09-02 08:22:45 +0000

    [EXPERIMENTAL] Create "ReviewFile" class (print PDF review files).
    git-svn-id: file:///media/data/gremy/svn/priv/repositories/svn/projet_0002/java/trunk@211 ecfe46be-1134-41ce-ba06-f4350a42f343

---
+++ src/org/jdhp/opencal/reviewfile/ReviewFile.java
@@ -0,0 +1,19 @@
+/*
+ * OpenCAL version 3.0
+ * Copyright (c) 2007,2008 Jérémie Decock
+ */
+
+package org.jdhp.opencal.reviewfile;
+
+/**
+ * 
+ * @author Jérémie Decock
+ *
+ */
+public class ReviewFile {
+	
+	public static void createPdfFile() {
+		// TODO
+	}
+	
+}
