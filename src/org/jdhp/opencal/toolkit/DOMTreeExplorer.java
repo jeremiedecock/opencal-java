351dff79d416dae9c8a88482ae9b95d7f9bbf680
gremy <gremy@ecfe46be-1134-41ce-ba06-f4350a42f343>
2008-09-02 17:31:24 +0000

    Add the "DOMTreeExplorer" toolkit.
    git-svn-id: file:///media/data/gremy/svn/priv/repositories/svn/projet_0002/java/trunk@212 ecfe46be-1134-41ce-ba06-f4350a42f343

---
+++ src/org/jdhp/opencal/toolkit/DOMTreeExplorer.java
@@ -0,0 +1,44 @@
+package org.jdhp.opencal.toolkit;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class DOMTreeExplorer {
+
+	/**
+	 * This method can be used to print a Node tree from a DOM document loaded in memory.
+	 * 
+	 * Example usage :
+	 * DOMTreeExplorer.printNodeTree(
+	 * 			this.element,
+	 * 			"Before removing old "tag" elements\n\n",
+	 * 			"\n************************\n\n");
+	 * 
+	 * @param node The node to inspect
+	 * @param header Some words printed before the tree
+	 * @param footer Some words printed after the tree
+	 */
+	public static void printNodeTree(Node node, String header, String footer) {
+		System.out.print(header);
+		
+		DOMTreeExplorer.printNodeTree(node, "");
+		
+		System.out.print(footer);
+	}
+	
+	/**
+	 * This method is used by printNodeTree to get a recursive call.
+	 * 
+	 * @param node The (sub)node to inspect
+	 * @param header Used to indent the output
+	 */
+	private static void printNodeTree(Node node, String header) {
+		System.out.println(header + node.getNodeName());
+		
+		NodeList nodes = node.getChildNodes();
+		for(int i=0 ; i<nodes.getLength() ; i++) {
+			DOMTreeExplorer.printNodeTree(nodes.item(i), header + "   ");
+		}
+	}
+	
+}
