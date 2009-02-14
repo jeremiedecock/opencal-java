7c562be3d741fa6ef95929b0d35d670c283772b0
gremy <gremy@ecfe46be-1134-41ce-ba06-f4350a42f343>
2009-02-14 23:35:17 +0000

    Corrige un bug dans la methode "toString()" : avant cette methode se contentait de concatener bêtement l'appel de "toString()" sur toutes ses cartes => il n'y avait pas d'élement racine et une directive "<?xml ... ?>" accompagnait chaque carte. Désormais un document DOM est créer pour génerer proprement la sortie XML.
    git-svn-id: file:///media/data/gremy/svn/priv/repositories/svn/projet_0002/java/trunk@367 ecfe46be-1134-41ce-ba06-f4350a42f343

--- src/org/jdhp/opencal/card/CardList.java
+++ src/org/jdhp/opencal/card/CardList.java
@@ -5,10 +5,24 @@
 
 package org.jdhp.opencal.card;
 
+import java.io.StringWriter;
 import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.jdhp.opencal.card.CardManipulator;
 import org.jdhp.opencal.card.Manipulable;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * 
@@ -49,12 +63,48 @@ public class CardList extends ArrayList<Card> implements Manipulable {
 	 * 
 	 */
 	public String toString() {
-		StringBuffer stringBuffer = new StringBuffer();
-		
-		for(int i=0 ; i<this.size() ; i++) {
-			stringBuffer.append(this.get(i).toString());
+		// Créer un document
+		Document document = null;
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db;
+		try {
+			db = dbf.newDocumentBuilder();
+			document = db.newDocument();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
 		}
 		
-		return stringBuffer.toString();
+		// Ajouter un noeud root au document
+		Element rootElement = document.createElement("root");
+		document.appendChild(rootElement);
+		
+		// Ajouter les elements de chaques cartes dans le rootElement
+		for(int i=0 ; i<this.size() ; i++) {
+			rootElement.appendChild(document.importNode(this.get(i).getElement(),true));
+		}
+		
+		// Convert the document
+		StringWriter stringWriter = new StringWriter();
+		
+		try {
+			// Make DOM source
+			Source domSource = new DOMSource(document);
+		
+			// Make output file
+			Result streamResult = new StreamResult(stringWriter);
+		
+			// Setup transformer
+			TransformerFactory factory = TransformerFactory.newInstance();
+			Transformer transformer = factory.newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+		
+			// Transformation
+			transformer.transform(domSource, streamResult);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return stringWriter.toString();
 	}
 }
