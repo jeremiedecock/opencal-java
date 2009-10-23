51aa46825a5bd39717f9162980478d370136b09a
gremy <gremy@ecfe46be-1134-41ce-ba06-f4350a42f343>
2009-10-23 08:33:19 +0000

    Remove 'Manipulable' interface (useless)
    git-svn-id: file:///media/data/gremy/svn/priv/repositories/svn/projet_0002/java/trunk@559 ecfe46be-1134-41ce-ba06-f4350a42f343

--- src/org/jdhp/opencal/card/CardList.java
+++ src/org/jdhp/opencal/card/CardList.java
@@ -20,7 +20,6 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.jdhp.opencal.card.CardManipulator;
-import org.jdhp.opencal.card.Manipulable;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -29,7 +28,7 @@ import org.w3c.dom.Element;
  * @author Jérémie Decock
  *
  */
-public class CardList extends ArrayList<Card> implements Manipulable {
+public class CardList extends ArrayList<Card> {
 
 	/**
 	 * 
