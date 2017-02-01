5e2fcf2058bdb572c9e7e003e4ad7dafebeb7d8b
Jeremie DECOCK <jd.jdhp@gmail.com>
2017-02-01 19:29:22 +0100

    Add a test script to check whether CSS files are well loaded.

---
+++ tools/check_css.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env jython
+# -*- coding: utf-8 -*-
+
+# OpenCAL
+# Copyright (c) 2007,2008,2009,2010,2011,2012,2016,2017 Jérémie DECOCK <jd.jdhp@gmail.com> (www.jdhp.org)
+
+# Usage:
+#
+#     jython -Dpython.path="./bin/" check_css.py
+#
+# Declare the CLASSPATH variable in your environment to avoid the "-cp"
+# option, e.g. add the following line to the end of your bashrc or
+# bash_profile:
+#
+#     export CLASSPATH="/YOUR/PATH/TO/OPENCAL/bin:$CLASSPATH"
+#
+# For Unix users, don't use the shorten notation of your home path ("~/") in
+# CLASSPATH definition, it won't work!
+#
+# Then you can simply run this script with:
+#
+#     jython check_css.py
+#
+# or simply:
+#
+#     ./check_css.py
+
+# Java import
+from java.net import URI
+
+# OpenCAL import
+from org.jdhp.opencal.ui.css import CSS
+
+def main():
+    html = CSS.EDITABLE_BROWSER_CSS
+    print(html)
+
+if __name__ == '__main__':
+    main()
+
