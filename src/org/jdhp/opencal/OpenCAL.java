c94ab3f43b6c5b94d215642f93ba1f3dada612fa
Jeremie DECOCK <jd.jdhp@gmail.com>
2018-03-07 00:03:55 +0100

    Update the copyright notice.

--- src/org/jdhp/opencal/OpenCAL.java
+++ src/org/jdhp/opencal/OpenCAL.java
@@ -11,20 +11,20 @@ import org.jdhp.opencal.model.professor.Professor;
 
 /**
  * OpenCAL
- * 
+ *
  * @author Jérémie Decock
  * @version 3.0
  */
 public class OpenCAL {
 
     public final static String PROGRAM_VERSION = "3.0.0";
-    
+
     public final static String PROGRAM_NAME = "OpenCAL";
-    
-    public final static String COPYRIGHT = "Copyright © 2007,2008,2009,2010,2011,2012 Jérémie DECOCK";
-    
+
+    public final static String COPYRIGHT = "Copyright © 2007,2008,2009,2010,2011,2012,2016,2017,2018 Jérémie DECOCK";
+
     public final static String WEB_SITE = "http://www.jdhp.org";
-    
+
     public static Professor professor = null;        // TODO
     public static PersonalKnowledgeBase pkb = null;  // TODO
     public static CardCollection cardCollection = null;
