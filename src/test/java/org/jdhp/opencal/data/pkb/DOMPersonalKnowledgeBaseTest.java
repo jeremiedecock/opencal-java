08f2da036fbf9b953b6e8f569ddc5c5a7886e90b
Jeremie DECOCK <jd.jdhp@gmail.com>
2018-03-15 23:01:22 +0100

    Fix a unit test (on PKB parser).

--- src/test/java/org/jdhp/opencal/data/pkb/DOMPersonalKnowledgeBaseTest.java
+++ src/test/java/org/jdhp/opencal/data/pkb/DOMPersonalKnowledgeBaseTest.java
@@ -6,21 +6,40 @@ import org.jdhp.opencal.model.card.Card;
 import org.jdhp.opencal.model.cardcollection.CardCollection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 class DOMPersonalKnowledgeBaseTest {
 
+    /**
+     * Default DOM parser puts some unicode caracters like 𝔓 or 𝓔 outside the CDATA section, e.g.:
+     *     <![CDATA[The following caracter ]]>𝓔<![CDATA[ is put outside the cdata section with DOM parser.]]>
+     * instead of
+     *     <![CDATA[The following caracter 𝓔 is put outside the cdata section with DOM parser.]]>
+     *
+     * This test ensures that both versions gives the same results with DOMPersonalKnowledgeBase.
+     */
     @Test
-    void myFirstTest() {
-        //String path = DOMPersonalKnowledgeBaseTest.class.getResourceAsStream(name);
+    @DisplayName("Test Unicode inside CDATA == Unicode outside CDATA")
+    void xmlCDataUnicodeTest() throws PersonalKnowledgeBaseException {
+        //// See https://howtoprogram.xyz/2017/01/17/read-file-and-resource-in-junit-test/
+        //File file = new File("src/test/resources/unicode.pkb");
+        //assertTrue(file.exists());
 
-        //CardCollection cardCollection = load(URI uri) throws PersonalKnowledgeBaseException
-        //for(Card card : cardCollection) {
-        //    assertEquals(card.getQusetion(), card.getAnswer());
-        //}
+        // See https://howtoprogram.xyz/2017/01/17/read-file-and-resource-in-junit-test/
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        File file = new File(classLoader.getResource("xml_cdata_unicode.pkb").getFile());
+        assertTrue(file.exists());
 
-        assertEquals(2, 1 + 1, "1 + 1 should equal 2");
+        PersonalKnowledgeBase pkbInstance = new DOMPersonalKnowledgeBase();
+        CardCollection cardCollection = pkbInstance.load(file.toURI());
+        for(Card card : cardCollection) {
+            assertEquals(card.getQuestion(), card.getAnswer());
+        }
     }
 
 }
